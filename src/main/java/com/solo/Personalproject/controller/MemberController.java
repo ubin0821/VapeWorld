4c084196424a3e3736e86cc64a35b4881523d95b
ubin0821 <hello.git.z9096@naver.com>
2024-07-22 18:16:45 +0900

    240722 연습 1일 -> Mapping 수정

--- src/main/java/com/solo/Personalproject/controller/MemberController.java
+++ src/main/java/com/solo/Personalproject/controller/MemberController.java
@@ -2,23 +2,29 @@ package com.solo.Personalproject.controller;
 
 import com.solo.Personalproject.dto.MemberFormDto;
 import com.solo.Personalproject.entity.Member;
+import com.solo.Personalproject.service.MailService;
 import com.solo.Personalproject.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/member")
+@RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
+    private final MailService mailService;
+
+
+    String confirm = "";
+    boolean confirmCheck = false;
 
     @GetMapping(value = "/login")
     public String loginMember() {
@@ -54,5 +60,22 @@ public class MemberController {
         }
         return "member/anno";
     }
+    @PostMapping("/{email}/emailConfirm")
+    public @ResponseBody ResponseEntity emailConfrim(@PathVariable("email") String email)
+            throws Exception{
+        System.out.println("email");
 
+        confirm = mailService.sendSimpleMessage(email);
+        System.out.println("인증코드 : "+ confirm);
+        return new ResponseEntity<String>("인증 메일을 보냈습니다.", HttpStatus.OK);
+    }
+    @PostMapping("/{code}/codeCheck")
+    public @ResponseBody ResponseEntity codeConfirm(@PathVariable("code")String code)
+            throws Exception {
+        if (confirm.equals(code)) {
+            confirmCheck = true;
+            return new ResponseEntity<String>("인증 성공하였습니다.", HttpStatus.OK);
+        }
+        return new ResponseEntity<String>("인증 코드를 올바르게 입력해주세요.", HttpStatus.BAD_REQUEST);
+    }
 }
